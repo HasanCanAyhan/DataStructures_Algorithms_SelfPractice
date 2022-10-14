f0997d654f7eebbe3f1a63d7fafb6cc20609e5c2
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-14 21:21:14 +0200

    DataStructures and Algorithms

--- m6-Queues_And_Hashing/src/main/java/queues/CollectionsQueue.java
+++ m6-Queues_And_Hashing/src/main/java/queues/CollectionsQueue.java
@@ -1,10 +1,26 @@
 package queues;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CollectionsQueue {
 
     public static void main(String[] args) {
 
+        //FIFO
 
+        MyQueue<Integer> mq = new MyQueue<>();
+        mq.enqueue(1);
+        mq.enqueue(2);
+
+        System.out.println(mq.dequeue());//1
+        System.out.println(mq.dequeue());//2
+
+        Queue<Integer> cq = new LinkedList<>();
+        cq.add(1);
+        cq.add(3);
+        System.out.println(cq.remove());
+        System.out.println(cq.remove());
 
 
     }
