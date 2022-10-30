ccbaabfb2cf815caa5d1be33fe45d49fcdcae462
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-30 19:18:59 +0100

    DataStructures and Algorithms

---
+++ m7-Trees/src/main/java/TreeApp.java
@@ -0,0 +1,15 @@
+public class TreeApp {
+    public static void main(String[] args) {
+        MyTree tree=new MyTree();
+        int[] numbers=new int[] { 10, 6, 8, 20, 4, 9, 5, 17, 42, 47, 29};
+        for (int i = 0; i <11; i++) {
+            tree.insert(numbers[i]);
+        }
+
+
+       VisualizeTree.printTree(tree.root,null,false);
+
+
+
+    }
+}
