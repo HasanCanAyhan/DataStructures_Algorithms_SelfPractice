16e9b425671d0853b6650ac7e2690e44a4a5b081
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-12-09 18:05:45 +0100

    DataStructures and Algorithms

---
+++ m8-Heap/src/main/java/KthLargest.java
@@ -0,0 +1,25 @@
+public class KthLargest {
+
+    public static void main(String[] args) {
+
+        int[] num = {3,2,1,5,6,4};
+        System.out.println(getKthLargest(num,2));
+    }
+
+    public static int getKthLargest(int[] array, int k){
+
+        //create a heap
+        MyHeap heap = new MyHeap(array.length);
+        for (int i = 0; i < array.length ; i++) {
+            heap.insert(array[i]);
+        }
+
+        for (int i = 0; i < k-1 ; i++) {
+            heap.remove();
+        }
+
+        return heap.peek();
+
+    }
+
+}
