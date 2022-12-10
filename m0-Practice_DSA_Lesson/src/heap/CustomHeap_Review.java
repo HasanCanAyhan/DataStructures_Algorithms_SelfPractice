5cdbaf1d7d6269c8461f851beace21e822052f86
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-12-10 11:37:57 +0100

    DataStructures and Algorithms

---
+++ m0-Practice_DSA_Lesson/src/heap/CustomHeap_Review.java
@@ -0,0 +1,146 @@
+package heap;
+
+import java.util.NoSuchElementException;
+
+public class CustomHeap_Review {
+
+    int[] items;
+    int size;
+
+    public CustomHeap_Review(int capacity) {
+        items = new int[capacity];
+        size = 0;//default
+    }
+
+    public int peek() {
+
+        if (size == 0) throw new NoSuchElementException(); // there is no value in the array
+
+        return items[0];// everytime index zero is root
+
+    }
+
+    public void insert(int value) {
+
+        if (size == items.length) throw new NoSuchElementException(); // bcs it is full
+        else {
+            items[size++] = value;
+            bubbleUp();
+        }
+
+    }
+
+    private void bubbleUp() {
+
+        int startIndex = size - 1;
+
+        while (startIndex > 0 && items[startIndex] > items[parentIndex(startIndex)]) {
+            swap(startIndex, parentIndex(startIndex));
+            //assign start index to parent index
+            startIndex = parentIndex(startIndex);//to iterate, like curr = curr.next
+        }
+
+    }
+
+    private void swap(int startIndex, int parentIndex) {
+
+        int temp = items[startIndex];
+        items[startIndex] = items[parentIndex];
+        items[parentIndex] = temp;
+
+    }
+
+    private int parentIndex(int startIndex) {
+        return (startIndex - 1) / 2;
+    }
+
+
+    public int remove() {
+        int result = 0;
+        if (size == 0) throw new NoSuchElementException();
+        else {
+            //change the lastItem with root
+            result = items[0];// root
+            items[0] = items[--size];
+            bubbleDown();
+
+        }
+
+        return result;
+
+    }
+
+    private void bubbleDown() {
+
+        int startIndex = 0;
+        while (startIndex < size) {
+        int leftChildIndex = findLeftIndex(startIndex);
+        int rightChildIndex = findRightIndex(startIndex);
+
+        boolean hasLeftChild = hasLeftChild(leftChildIndex);
+        boolean hasRightChild = hasRightChild(rightChildIndex);
+
+
+
+            if (hasLeftChild && hasRightChild) {
+
+                if (items[leftChildIndex] > items[rightChildIndex]) {
+
+
+                        swap(startIndex, leftChildIndex);
+                        startIndex = leftChildIndex;
+
+
+                } else {
+
+
+                        swap(startIndex, rightChildIndex);
+                        startIndex = rightChildIndex;
+
+                }
+
+            }else if (hasLeftChild && !hasRightChild){
+
+                    swap(startIndex, leftChildIndex);
+                    startIndex = leftChildIndex;
+
+            } else if (!hasLeftChild && hasRightChild) {
+
+                    swap(startIndex, rightChildIndex);
+                    startIndex = rightChildIndex;
+            }
+
+        }
+
+
+    }
+
+    private boolean hasRightChild(int rightChildIndex) {
+        return rightChildIndex <= size;
+    }
+
+    private boolean hasLeftChild(int leftIndex) {
+
+        return leftIndex <= size;
+
+    }
+
+    private int findRightIndex(int startIndex) {
+        return (startIndex * 2) + 2;
+    }
+
+    private int findLeftIndex(int startIndex) {
+        return (startIndex * 2) + 1;
+    }
+
+
+    public void printHeap() {
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i] + ", ");
+        }
+
+        System.out.println();
+    }
+
+}
--- m0-Practice_DSA_Lesson/src/heap/HeapApp.java
+++ m0-Practice_DSA_Lesson/src/heap/HeapApp.java
@@ -18,9 +18,16 @@ public class HeapApp {
 
 
         heap.printHeap();
-       // heap.remove();
+        int remove1 = heap.remove();
+        System.out.println("remove1 = " + remove1);
+        int remove2 = heap.remove();
+        System.out.println("remove2 = " + remove2);
+        int remove3 = heap.remove();
+        System.out.println("remove3 = " + remove3);
+        int remove4 = heap.remove();
+        System.out.println("remove4 = " + remove4);
 
-       // heap.printHeap();
+        heap.printHeap();
 
 
 
@@ -28,7 +35,28 @@ public class HeapApp {
         System.out.println("------------------------------------------------------");
 
 
+        CustomHeap_Review customHeap_review = new CustomHeap_Review(10);
 
+        customHeap_review.insert(61);
+        customHeap_review.insert(120);
+        customHeap_review.insert(52);
+        customHeap_review.insert(45);
+        customHeap_review.insert(81);
+
+        customHeap_review.printHeap();
+
+        int remove_first = customHeap_review.remove();
+        System.out.println("remove1 = " + remove_first);
+        customHeap_review.printHeap();
+        int remove_second = customHeap_review.remove();
+        System.out.println("remove2 = " + remove_second);
+        customHeap_review.printHeap();
+        int remove_third = customHeap_review.remove();
+        System.out.println("remove3= " + remove_third);
+        customHeap_review.printHeap();
+        int remove_forth = customHeap_review.remove();
+        System.out.println("remove4= " + remove_forth);
+        customHeap_review.printHeap();
 
 
     }
