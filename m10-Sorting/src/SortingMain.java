1c3511cf2fb7f472da051237b28e2e957525aafb
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-12-22 18:59:19 +0100

    DataStructures and Algorithms

---
+++ m10-Sorting/src/SortingMain.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SortingMain {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {7,2,4,6,1,9,-1};
+        System.out.println(Arrays.toString(BubbleSort.bubbleSort(numbers))); //[-1, 1, 2, 4, 6, 7, 9]
+
+        System.out.println("-------------------------------");
+
+        System.out.println(Arrays.toString(SelectionSort.selectionSort(numbers)));//[-1, 1, 2, 4, 6, 7, 9]
+    }
+
+}
